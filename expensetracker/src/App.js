05e544aaa94c173315e2e8321c3ed0e7d4013811
Aditya <adi.bhardwaj3rd@gmail.com>
2021-10-31 12:50:06 +0530

    Completed Core React Functionality

--- expensetracker/src/App.js
+++ expensetracker/src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
-function App() {
+const INTIAL_EXPENSES = [
+  {
+    id: "e1",
+    title: "Car Insurance",
+    amount: 345.2,
+    date: new Date(2020, 5, 23),
+  },
+  {
+    id: "e2",
+    title: "TV Cable",
+    amount: 143.34,
+    date: new Date(2021, 2, 20),
+  },
+  {
+    id: "e3",
+    title: "Car Insurance",
+    amount: 34.3,
+    date: new Date(2021, 8, 13),
+  },
+  {
+    id: "e4",
+    title: "New Desktop",
+    amount: 433.4,
+    date: new Date(2019, 3, 11),
+  },
+];
+
+const App = () => {
+  
+  const [dynamicExpense, setDynamicExpense] = useState(INTIAL_EXPENSES);
+  //React createElement (OLD WAY)
+  // React.createElement(
+  //   "div",
+  //   {},
+  //   React.createElement("h1", {}, "Let's get started!"),
+  //   React.createElement(Expenses, {items: expenses})
+  // );
+
+  //JSX Code
+  const addExpenseDataHandler = (expense) => {
+    //clean way of updating our state when it is based on an older snapshot of the same data
+    setDynamicExpense(prevExpenses => {
+      return [expense, ...prevExpenses];
+    });
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NewExpense onAddExpense={addExpenseDataHandler}/>
+      <Expenses items={dynamicExpense} />
     </div>
   );
 }
---
+++ expensetracker/src/components/Expenses/Expenses.js
@@ -0,0 +1,67 @@
+import React, { useState } from "react";
+import Card from "../UI/Card";
+import ExpenseFilter from "./ExpenseFilter";
+import "./Expenses.css";
+import ExpensesList from "./ExpensesList";
+
+const Expenses = (props) => {
+  const [enteredYear, setEnteredYear] = useState("2020");
+  const filterChangeHandler = (selectedYear) => {
+    setEnteredYear(selectedYear);
+  };
+
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === enteredYear;
+  });
+
+  // 3rd way of doing
+  // let expensesContent = <p>No Expenses Found</p>;
+  // if (filteredExpenses.length > 0) {
+  //   expensesContent = filteredExpenses.map((expense) => (
+  //     <ExpenseItem
+  //       key={expense.id}
+  //       title={expense.title}
+  //       amount={expense.amount}
+  //       date={expense.date}
+  //     />
+  //   ));
+  // }
+  return (
+    <div>
+      <Card className="expenses">
+        <ExpenseFilter
+          selected={enteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+        <ExpensesList items={filteredExpenses}/>
+        {/* 2nd way of doing */}
+        {/* {filteredExpenses.length === 0 && <p> No Expenses Found </p>}
+        {filteredExpenses.length > 0 &&
+          filteredExpenses.map((expense) => (
+            <ExpenseItem
+              key={expense.id}
+              title={expense.title}
+              amount={expense.amount}
+              date={expense.date}
+            />
+          ))} */}
+
+        {/* 1st way of doing */}
+        {/* {filteredExpenses.length === 0 ? (
+          <p>No Expenses Found</p>
+        ) : (
+          filteredExpenses.map((expense) => (
+            <ExpenseItem
+              key={expense.id}
+              title={expense.title}
+              amount={expense.amount}
+              date={expense.date}
+            />
+          ))
+        )} */}
+      </Card>
+    </div>
+  );
+};
+
+export default Expenses;
